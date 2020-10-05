6afc910c79eb72b9febcc9df217bb1fffbfd0bfd
Daniel Araújo Costa de Aquino <70762869+AquinoProjects@users.noreply.github.com>
2020-10-04 23:46:38 -0300

    Update TelaConsultarResultados.java

--- src/interfacejava/TelaConsultarResultados.java
+++ src/interfacejava/TelaConsultarResultados.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 
+ * 
+ * 
  */
 package interfacejava;
 import Modelo.ModeloResultadosTabela;
@@ -9,7 +9,7 @@ import Modelo.Resultados;
 import javax.swing.JOptionPane;
 /**
  *
- * @author htaeaquino
+ * @author Daniel. A.C de Aquino
  */
 public class TelaConsultarResultados extends javax.swing.JFrame {
 
